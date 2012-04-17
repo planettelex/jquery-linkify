be556a88f9c032b9a97c72e3f2a2813c490eba93
Dan Hansen <dan@planettelex.net>
2012-04-17 12:32:23 -0600

    Updated the JS artifact name

--- Webroot/js/jquery-pt-linkify.js
+++ Webroot/js/jquery-pt-linkify.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010 Planet Telex Inc. all rights reserved.
+ * Copyright (c) 2012 Planet Telex Inc. all rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  * @fileOverview Contains the Linkify plugin code.
  * @author <a href="mailto:dan@planettelex.net">Dan Hansen</a>
